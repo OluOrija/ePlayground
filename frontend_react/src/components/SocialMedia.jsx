8f0ae0180da8c5763de144fea5440818bdec1102
Olusegun Orija <oluorija@gmail.com>
2024-10-21 18:09:18 +0100

    Adding youtube link

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { FaChessBishop, FaGithub } from 'react-icons/fa';
 import { SiLichess } from 'react-icons/si';
+import { FaYoutube } from "react-icons/fa";
 
 const SocialMedia = () => (
   <div className="app__social">
+    <div>
+      <a href="https://www.youtube.com/@ekelola" target="_blank" rel="noreferrer">
+      <FaYoutube /></a>
+    </div>
     <div>
       <a href='https://github.com/OluOrija' target="_blank" rel="noreferrer">
       <FaGithub /></a>
